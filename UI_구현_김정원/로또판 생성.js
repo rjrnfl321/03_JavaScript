e4d07c6fb14c2e029411170d5668bb90be41fff4
rjrnfl321 <rjrnfl321@gmail.com>
2024-07-15 00:23:21 +0900

    1111

--- UI_구현_김정원/로또판 생성.js
+++ UI_구현_김정원/로또판 생성.js
@@ -6,23 +6,40 @@ const lottoBoard = document.querySelector("#lottoBoard");
 createBtn.addEventListener("click", () => {
 
 
-  for (let i = 1; i < 46; i++) {
-    const number = document.createElement("div");
-    number.classList.add("number");
-    number.textContent = i;
-    number.addEventListener("click", e =>{
-    
-     const count = document.querySelectorAll(".number").length;
-     if(count <= 5  || e.targer.classList.contains("number")){
-      e.target.classList.toggle("number");
-     } 
-     else{
-      alert("6개 까지 선택할 수 있음");
-     }
-    });
+for(let i= 1; i<=46; i++){
+  const number = document.createElement("div");
+  number.classList.add("number");
+  number.textContent = [i];
+  number.addEventListener("click", e => {
 
+    const count = document.querySelectorAll(".number").length;
+  })
+  
 
-    lottoBoard.innerHTML = "";
-    lottoBoard.append(number);
-  }
+}
+
 })
+
+
+
+
+//   for (let i = 1; i < 46; i++) {
+//     const number = document.createElement("div");
+//     number.classList.add("number");
+//     number.textContent = i;
+//     number.addEventListener("click", e =>{
+    
+//      const count = document.querySelectorAll(".number").length;
+//      if(count <= 5  || e.targer.classList.contains("number")){
+//       e.target.classList.toggle("number");
+//      } 
+//      else{
+//       alert("6개 까지 선택할 수 있음");
+//      }
+//     });
+
+
+//     lottoBoard.innerHTML = "";
+//     lottoBoard.append(number);
+//   }
+// })
